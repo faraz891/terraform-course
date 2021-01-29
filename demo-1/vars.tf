1efef3e5a960cda11d1fb05ec12a9094ef4bc90e
faraz891 <8448574+faraz891@users.noreply.github.com>
2021-01-29 17:07:07 +0100

    Update vars.tf

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,5 +1,5 @@
-variable "AKIASRSCO5YXA6RP6TNR" { }
-#variable "vtTGUQP2qK8VcHN6AFYpEzisQ4ogrWN168QcNMaF" { }
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
@@ -8,6 +8,6 @@ variable "AMIS" {
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    
   }
 }
